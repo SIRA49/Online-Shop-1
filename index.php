fda17528b35415212be48f1a5d1754d75eda7576
SIRA49 <akurozlike@gmail.com>
2022-01-21 14:39:55 +0900

    h

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<head>
+<title>あ</title>
+
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="home">
+
+<header>
+<h1 id="logo"><a href="index.php"><img src="images/logo.png" alt=""></a></h1>
+
+
+<?php include("./topmenu.php"); ?>
+
+
+</header>
+
+
+<div id="contents">
+
+<div id="main">
+
+<section id="new">
+
+<h2>General Information</h2>
+
+<dl>
+<dt>2021/01/2</dt>
+<dd>新メニュー"新春ブレンド"の追加<span class="newicon">NEW</span></dd>
+<dt>2020/12/20</dt>
+<dd>クリスマスギフト販売開始</dd>
+<dt>2020/11/16</dt>
+<dd>店舗オープン</dd>
+
+
+</dl>
+
+</section>
+
+</div>
+
+</div>
+
+
+<aside id="mainimg">
+<div class="slide0">slide0</div>
+<div class="slide1">slide1</div>
+<div class="slide2">slide2</div>
+<div class="slide3">slide3</div>
+</aside>
+
+
+
+
+
+</body>
+</html>
+
---
+++ topmenu.php
@@ -0,0 +1,7 @@
+  <nav id="menubar">
+    <ul>
+     　	　 <li><a href="./index.php">ホーム</a></li>
+     　	　 <li><a href="./index.php">ホーム</a></li>
+     　	　 <li><a href="./index.php">ホーム</a></li>
+    </ul>
+  </nav>
