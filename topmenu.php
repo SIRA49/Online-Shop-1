2d255903a3f9678b9e7566eaf6ca593ccd9351b9
SIRA49 <akurozlike@gmail.com>
2022-01-21 15:46:08 +0900

    Co-authored-by: 白竹 竜也 <bl19051@cc.it-hiroshima.ac.jp> Co-authored-by: 今井 健太 <bl19009@cc.it-hiroshima.ac.jp>

--- topmenu.php
+++ topmenu.php
@@ -1,7 +1,8 @@
   <nav id="menubar">
     <ul>
      　	　 <li><a href="./index.php">ホーム</a></li>
-     　	　 <li><a href="./index.php">ホーム</a></li>
-     　	　 <li><a href="./index.php">ホーム</a></li>
+     　	　 <li><a href="./index.php">メニュー</a></li>
+     　	　 <li><a href="./index.php">店舗情報</a></li>
+         <li><a href="./index.php">お問い合わせ</a></li>
     </ul>
   </nav>
