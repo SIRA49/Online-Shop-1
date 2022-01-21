269a3b3f97ba890f6b3133047448ee70c209e85e
SIRA49 <akurozlike@gmail.com>
2022-01-21 21:32:56 +0900

    あ

--- about.php
+++ about.php
@@ -41,7 +41,8 @@
 </tr>
 <tr>
 <th>地図</th>
-<td><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6085.649291767679!2d132.20606115406923!3d34.14476639462967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3545356461c89499%3A0x21fec904af6f589b!2z44Kw44Op44Oz44OJ44OB44O844OV!5e0!3m2!1sja!2sjp!4v1642767358385!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+<td><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6085.649291767679!2d132.20606115406923!3d34.14476639462967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3545356461c89499%3A0x21fec904af6f589b!2z44Kw44Op44Oz44OJ44OB44O844OV!5e0!3m2!1sja!2sjp!4v1642767358385!5m2!1sja!2sjp
+  " width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </td>
 </tr>
 
