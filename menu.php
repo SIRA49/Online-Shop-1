cc159fb6bf15d6285dc161255ef613ce43e9aee1
SIRA49 <akurozlike@gmail.com>
2022-01-21 20:24:37 +0900

    a

--- menu.php
+++ menu.php
@@ -62,69 +62,21 @@
   $cnt = mysqli_num_rows($result);
   if ($cnt == 0) {
     echo "<b>ご指定のお部屋は只今準備ができておりません</b>";  
-  } else { }
+  } else { 
 ?>
 
-<h3 class="c">自家製ブレンド　</h3>
+<p>
+    コーヒー嫌いです。
+</p>
 
-	<div class="box1 ブレンド">
-	<figure><img src="images/b1.jpg" alt=""></figure>
-	<h4>自家製ブレンド　250ｇ　750円</h4>
-	<p>珈琲ならではの酸味がお楽しみいただける定番の商品です。</p>
-        <p>配合にはコロンビア,ブラジル,モカ,グァテマラを使用しております。</p>
-        
-	<span class="mark">人気</span>
-	
-	<figure><img src="images/b1.jpg" alt=""></figure>
-	<h4>新春ブレンド　250ｇ　940円</h4>
-	<p>良質な豆を使用した新春にふさわしい、超買い得ブレンドです♪</p>
-        <p>味・コク・香りのよさはまさに絶品。</p>
-	<span class="mark">人気</span>
-	</div>
+<table>
+            <th>お部屋名称</th>
+            <th>お部屋タイプ</th>
+            <th>一泊料金<br>（部屋単位）</th>
+            <th colspan="2">お部屋イメージ</th>
+</table>
+ }
 
-	
-
-<h3 class="c">ストレート豆</h3>
-
-	<div class="box1 ストレート">
-	<figure><img src="images/koro.jpg" alt=""></figure>
-	<h4>コロンビア　250ｇ　830円</h4>
-	<p>マイルドコーヒーの代表。優れた香り、コクを持っています。</p>
-        <p>「エルパライソ・スプレモ」のプレミア品です。</p>
-	<span class="mark">人気</span>
-	</div>
-
-	<div class="box1 ストレート">
-	<figure><img src="images/bura.jpg" alt=""></figure>
-	<h4>ブラジル　250ｇ　860円</h4>
-	<p>当店では、ストレートにもブレンドにも欠かせないブラジル産コーヒーを、農園と直接取引を行う『契約農園制』を始めました。</p>
-        <p>ミナスジェライ州のセラード大丘陵から送られる、バランスがとれクセのない、珈琲をお楽しみください。</p>
-	
-	</div>
-
-	<div class="box1 ストレート">
-	<figure><img src="images/moka.jpg" alt=""></figure>
-	<h4>モカ　250ｇ　960円</h4>
-	<p>ウォシュドモカならではの『ワインフレーバー・フルボディー・上品な口当たり』等がスペシャルティーコーヒーとして評価されています。</p>
-	</div>
-
-<div class="box1 ストレート">
-	<figure><img src="images/gua.jpg" alt=""></figure>
-	<h4>グァテマラ　250ｇ　830円</h4>
-	<p>香り高いコーヒーで知られる有名産地アンティグア地区。</p>
-        <p>香りが高く、良質の酸味とコクが特徴です。</p>
-	</div>
-
-<div class="box1 ストレート">
-	<figure><img src="images/buru.jpg" alt=""></figure>
-	<h4>ブルーマウンテン　250ｇ　4590円</h4>
-	<p>ブルーマウンテンの最高規格【No.1】は上品でやわらかい風味が特徴で、バランスの良さは絶品です。</p>
-<p>ブルーマウンテンはコーヒーの最高級品とされています。</p>
-	<span class="mark">人気</span>
-	</div>
-
-
-	</section>
 
 <section class="ofh box">
 	
