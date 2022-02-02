bfb2ab007a75cd752a3bb69c15f524c26533ea8f
SIRA49 <akurozlike@gmail.com>
2022-02-02 21:55:28 +0900

    j

--- dbConfig.php
+++ dbConfig.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER", "localhost");     // サーバ
 define("DB_NAME", "cafe");  // データベース
-define("DB_USER", "shiratake");          // ユーザ
+define("DB_USER", "root");          // ユーザ
 define("DB_PASS", "pass");            // パスワード
 
 define("OWNER_ID", "owner");			// オーナーログイン用ID
