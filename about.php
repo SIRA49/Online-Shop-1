276ef6db603d06a6b0fae9ebee6a089c95bd6301
SIRA49 <akurozlike@gmail.com>
2022-02-02 21:48:14 +0900

    j

--- about.php
+++ about.php
@@ -41,8 +41,7 @@
 </tr>
 <tr>
 <th>地図</th>
-<td><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3293.245683006111!2d132.34187541541817!3d34.369671080520725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355abb8f3c90c255%3A0x13cae7ee0b809136!2z44CSNzMxLTUxNDMg5bqD5bO255yM5bqD5bO25biC5L2Q5Lyv5Yy65LiJ5a6F77yS5LiB55uu77yR4oiS77yR!5e0!3m2!1sja!2sjp!4v1611764692118!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-</td>
+<td><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d32406.510788167376!2d132.17120705677323!3d34.160207955316736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3545343e6b6e661d%3A0x6654b3733cf09708!2z6Yym5biv5qmL!5e0!3m2!1sja!2sjp!4v1643805999604!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></td>
 </tr>
 
 
