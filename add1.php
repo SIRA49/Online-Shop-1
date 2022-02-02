980e27ce9a3405c5bb2a0b5b381340d69221b23f
SIRA49 <akurozlike@gmail.com>
2022-02-02 13:44:03 +0900

    k

--- add1.php
+++ add1.php
@@ -1,4 +1,28 @@
 <?php
+  session_start();
+  $dnameErr = "";
+  if (isset($_SESSION['errMsg']['dname'])) {
+    $dnameErr = "<span style='color: red;'>" . $_SESSION['errMsg']['dname'] ."</span>";
+  }
+  unset($_SESSION['errMsg']); // すべてのエラーメッセージをクリア
+  require_once('./dbConfig.php');
+  $link = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  if ($link == null) {
+    die("接続に失敗しました：" . mysqli_connect_error());
+  }
+  mysqli_set_charset($link, "utf8");
+
+  $sql = "SELECT bean_name  FROM cafe,cafe_type  WHERE  cafe.type_id = cafe_type.type_id";
+  $result = mysqli_query($link, $sql);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  $Name = $row['bean_name'];
+  if (isset($_SESSION['reserve']['dname']) == true) {
+      $dname = $_SESSION['reserve']['dname'];
+  }
+?>
+
+
+<?php
 
 $user = 'shiratake';
 $pass = 'pass';
